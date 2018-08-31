b467d37daa560a2185b5939875517e066d317df4
Darren Liu <dliu9999@gmail.com>
2018-08-31 12:05:52 -0700

    fixed columns

--- vons_json.py
+++ vons_json.py
@@ -54,7 +54,7 @@ while True:
 	table_columns = [i + " TEXT" for i in to_keep]
 	table = "CREATE TABLE popular(ID TEXT,{0},day TEXT)".format(",".join(table_columns))
 	query = "INSERT INTO popular(ID,{0},day) VALUES (%s{1})"
-	query = query.format(",".join(to_keep), ",%s" * len(to_keep))
+	query = query.format(",".join(to_keep), ",%s" * (len(to_keep) + 1))
 
 	#create table
 	if times_ran == 0:
