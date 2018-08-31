4354ca31d13a4f3e626b651ecc1b874bcd6eedb8
Darren Liu <dliu9999@gmail.com>
2018-08-30 17:06:01 -0700

    fixed syntax

--- vons_json.py
+++ vons_json.py
@@ -45,7 +45,7 @@ conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
 items = list(data.values())[0].keys()
 columns = list(items)
-query = "INSERT INTO popular(ID,{0}) VALUES (?{1})"
+query = "INSERT INTO popular(ID,{0}) VALUES (%s{1})"
 query = query.format(",".join(columns), ",?" * len(columns))
 table_columns = [i + " TEXT" for i in columns]
 table = "CREATE TABLE popular(ID TEXT,{0})".format(",".join(table_columns))
