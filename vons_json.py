383e3bddeac064f5a14d1c21a56dae07032fbc4f
Darren Liu <dliu9999@gmail.com>
2018-08-31 11:34:28 -0700

    added loop and date

--- vons_json.py
+++ vons_json.py
@@ -18,59 +18,64 @@ ZIP = 92092
 KEY = 'promo'
 VALUE = 'popular-items-1750'
 
-# data = {'resourcePath': '/content/shop/vons/en/welcome/jcr:content/root/responsivegrid/column_control/par_0/two_column_zip_code_', 'zipcode': 92092}
-data = {'zipcode':ZIP}
-payload = {'key':KEY, 'value':VALUE}
+while True:
+	# data = {'resourcePath': '/content/shop/vons/en/welcome/jcr:content/root/responsivegrid/column_control/par_0/two_column_zip_code_', 'zipcode': 92092}
+	data = {'zipcode':ZIP}
+	payload = {'key':KEY, 'value':VALUE}
 
 
-### Session start ###
-s = requests.Session()
+	### Session start ###
+	s = requests.Session()
 
-s.post(url, data=data)
-r = s.get(base, params=payload)
+	s.post(url, data=data)
+	r = s.get(base, params=payload)
 
-results = r.json()['products']
+	results = r.json()['products']
 
 
-### Index by id ###
-data = {}
-for item in results:
-    data[item.pop('id')] = item
+	### Index by id ###
+	data = {}
+	for item in results:
+	    data[item.pop('id')] = item
 
-#set up database
-DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+	#set up database
+	DATABASE_URL = os.environ['DATABASE_URL']
+	conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-#create new table
-cur = conn.cursor()
+	#create new table
+	cur = conn.cursor()
 
-#get column names
-items = list(data.values())[0].keys()
-columns = list(items)
+	#get column names
+	items = list(data.values())[0].keys()
+	columns = list(items)
 
-#create strings for cur.execute
-table_columns = [i + " TEXT" for i in columns]
-table = "CREATE TABLE popular(ID TEXT,{0})".format(",".join(table_columns))
-query = "INSERT INTO popular(ID,{0}) VALUES (%s{1})"
-query = query.format(",".join(columns), ",%s" * len(columns))
-print(table)
+	#create strings for cur.execute
+	table_columns = [i + " TEXT" for i in columns]
+	table = "CREATE TABLE popular(ID TEXT,{0},day TEXT)".format(",".join(table_columns))
+	query = "INSERT INTO popular(ID,{0},day TEXT) VALUES (%s{1})"
+	query = query.format(",".join(columns), ",%s" * len(columns))
+	to_keep = ["price", "name", "sellByWeight", "unitOfMeasure", "aisleName", "shelfName", "pricePer","day"]
 
-#write to postgres
-cur.execute(table)
-for ID, rest in data.items():
-	keys = (ID,) + tuple(rest[c] for c in columns)
-	cur.execute(query, keys)
+	#create table
+	if times_ran == 0:
+		cur.execute(table)
+		for name in columns:
+			if name not in to_keep:
+				to_delete = "ALTER TABLE popular DROP COLUMN {0}".format(name)
+				cur.execute(to_delete)
+		times_ran = 1
 
-#delete columns
-to_keep = ["price", "name", "sellByWeight", "unitOfMeasure", "aisleName", "shelfName", "pricePer"]
-for name in columns:
-	if name not in to_keep:
-		to_delete = "ALTER TABLE popular DROP COLUMN {0}".format(name)
-		cur.execute(to_delete)
+	#insert data
+	current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+	for ID, rest in data.items():
+		keys = (ID,) + tuple(rest[c] for c in columns if c not in to_keep) + (,current_date)
+		cur.execute(query, keys)
 
-#close database
-conn.commit()
-cur.close()
-conn.close()
-print("worked")
+	#close database
+	conn.commit()
+	cur.close()
+	conn.close()
+	print("worked")
 
+	#sleep for half a week
+	time.sleep(5040)
